aa561af45bb777bec8cd90549bf0252f295cd290
Gameunkulus <bugr24-8@mail.ru>
2023-05-17 12:27:15 +0300

    programm redacted

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int rubles = 13676;
+        int miles = 20;
+        int bonuses = rubles/miles;
+        System.out.println(bonuses);
+    }
+}
