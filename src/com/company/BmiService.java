2da917e324d9ddd5eda9dfa6a3f054a88ba04ed5
Gameunkulus <bugr24-8@mail.ru>
2023-05-21 13:47:46 +0300

    program2 redacted 3

--- src/com/company/BmiService.java
+++ src/com/company/BmiService.java
@@ -2,9 +2,9 @@ package com.company;
 
 public class BmiService {
 
-    public static int calculate(double heigthM, double weigthKG){
-        double index = (weigthKG/(heigthM * heigthM));
-        return (int)index;
+    public static int calculate(double heigthM, double weigthKG) {
+        double index = (weigthKG / (heigthM * heigthM));
+        return (int) index;
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double heigh = 1.87;
+        double height = 1.87;
         double weigth = 98;
-        int bmi = service.calculate(heigh,weigth);
+        int bmi = service.calculate(height, weigth);
         System.out.println(bmi);
     }
 }
